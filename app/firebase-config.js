d4a7fbb16bef4e27ba058dea0993474bac620564
Joao Meio <83929486+joaomeio@users.noreply.github.com>
2025-09-19 19:37:21 -0300

    Create firebase-config.js

---
+++ app/firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyD2s1MYGT6VKsNpHITWskAfIMSq2f3l280",
+  authDomain: "gamblingchat-3b72e.firebaseapp.com",
+  projectId: "gamblingchat-3b72e",
+  storageBucket: "gamblingchat-3b72e.firebasestorage.app",
+  messagingSenderId: "72704465148",
+  appId: "1:72704465148:web:1598bcf0276a0a43245f79",
+  measurementId: "G-F2Q5Z4JZSG"
+};
